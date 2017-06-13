0ae652b0c4384079ced3c6b63ed95b25d75169ba
Heather Phelps <heather.phelps@gmail.com>
2017-06-13 14:06:57 -0400

    Updates to finding circles

--- tree.py
+++ tree.py
@@ -1,20 +1,30 @@
-#minimum spanning tree algorithym
+# minimum spanning tree algorithym
 
-G = { 'C': [('B', 5)],'A': [('B', 2)], 'B': [('A', 2), ('C',5)]}
+G = { 'C': [('B', 5)],'A': [('B', 2)], 'B': [('A', 2), ('C', 5)]}
+G = { 'C': [('B', 5)],
+	  'A': [('B', 2), ('D', 4)], 
+	  'B': [('A', 2), ('C', 5), ('D', 6)],
+	  'D': [('A', 4), ('B', 6)] }
+
+# need weight, source, and destination for each node pair
+# don't need to get each direction because this is an undirected graph
 
 def Question3(G):
 
-	# determine # of vertices ( minus 1 where to stop iteration )
-	v = len(G) - 1
-	#graph = sorted(G)
-	result = []
+	result = {} # empty dictionary to hold data
 	# make into list of all vertices and sort by weight
 	for h, i in G.items():
-		j = h
 		for p in i:
-			j = j + p[0] + str(p[1])
+			o = h + p[0] # vertices # key
+			# check to see if reversed edge already in result
+			r = o[::-1] #reverse the string
+			if r in result: continue # check if it's already there			
+			result[o] = p[1] # weight # value # add to dictionary
 
+	# sort result by weight
+	f = sorted(result.items(), key=lambda x:x[1]) # makes a list of tuples sorted by weight
+	# [('AB', 2), ('AD', 4), ('CB', 5)] # order of vertices doesn't matter - undirected graph
 
-	return j
+	return f
 
 print Question3(G)
