6bac576606a85e6c892df5657b0091aecff3b69d
Heather Phelps <heather.phelps@gmail.com>
2017-06-20 17:11:05 -0400

    Done.

--- ancestor.py
+++ ancestor.py
@@ -15,35 +15,56 @@
 
 # For example, one test case might be
 
-T = [[0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]]
+T = [[0, 1, 1, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]]
 r = 3
 n1 = 1
 n2 = 4
 
 
+def rootTest(r, holder):
+    if holder == r:
+        return True
+
+
 def question4(T, r, n1, n2):
 
     n1folks = []  # list to hold n1 parents
+    n2folks = []
 
     for t in T:
-        if t[n1] or t[n1] == 0:
+        if t[n1]:
             n1folks.append(T.index(t))
-            # now find the parent of that node
+            # now find the parent of that node until it hits root (r)
+            # test for root
             holder = T.index(t)
-            if t[holder] or t[holder] == 0:
-                print "the parent has a parent"
+            while not rootTest(r, holder):
+                for tp in T:
+                    # keep finding parents until root is identified
+                    if tp[holder]:  # find parent
+                        n1folks.append(T.index(tp))  # add parent to n1folks
+                        holder = T.index(tp)
+            # if t[holder] or t[holder] == 0:
+            #     print "the parent has a parent"
             # this needs to be recursive and call a function do...while
-    return n1folks
+        # else:
+        #     print 'No child at ' + str(n1)
+    for n in T:  # find second node parents
+        if n[n2]:
+            n2folks.append(T.index(n))
+            # now find the parent of that node until it hits root (r)
+            holder = T.index(n)
+            while not rootTest(r, holder):  # test for root
+                for np in T:
+                    # keep finding parents until root is identified
+                    if np[holder]:  # find parent
+                        n2folks.append(T.index(np))  # add parent to n2folks
+                        holder = T.index(np)
+    # commonA = set(n1folks).intersection(n2folks)
+    commonA = set(n1folks) & set(n2folks)
+    return list(commonA)
 
 
 print question4(T, r, n1, n2)
 
 
-# and the answer would be 3.
-
-# Method 1 (By Storing root to n1 and root to n2 paths):
-# Following is simple O(n) algorithm to find LCA of n1 and n2.
-# 1) Find path from root to n1 and store it in a vector or array.
-# 2) Find path from root to n2 and store it in another vector or array.
-# 3) Traverse both paths till the values in arrays are same. Return the
-# common element just before the mismatch.
+# and the answer would be 3
