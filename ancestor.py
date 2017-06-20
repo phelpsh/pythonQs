88786ceee75d95b73a9c763598e8e361abc62da7
Heather Phelps <heather.phelps@gmail.com>
2017-06-20 08:55:21 -0400

    Add files via upload

--- ancestor.py
+++ ancestor.py
@@ -15,15 +15,35 @@
 
 # For example, one test case might be
 
-T = [[0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]]
+T = [[0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 0, 0, 0], [1, 0, 0, 0, 1], [0, 0, 0, 0, 0]]
 r = 3
 n1 = 1
 n2 = 4
 
+
 def question4(T, r, n1, n2):
-	return T
+
+    n1folks = []  # list to hold n1 parents
+
+    for t in T:
+        if t[n1] or t[n1] == 0:
+            n1folks.append(T.index(t))
+            # now find the parent of that node
+            holder = T.index(t)
+            if t[holder] or t[holder] == 0:
+                print "the parent has a parent"
+            # this needs to be recursive and call a function do...while
+    return n1folks
+
 
 print question4(T, r, n1, n2)
 
 
 # and the answer would be 3.
+
+# Method 1 (By Storing root to n1 and root to n2 paths):
+# Following is simple O(n) algorithm to find LCA of n1 and n2.
+# 1) Find path from root to n1 and store it in a vector or array.
+# 2) Find path from root to n2 and store it in another vector or array.
+# 3) Traverse both paths till the values in arrays are same. Return the
+# common element just before the mismatch.
