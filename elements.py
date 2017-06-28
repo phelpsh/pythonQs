a6b10586b036afc5fa8b6a3240f84816a3ce8b8f
Heather Phelps <heather.phelps@gmail.com>
2017-06-28 08:06:58 -0400

    Done

--- elements.py
+++ elements.py
@@ -9,7 +9,7 @@
 
 class Node(object):
 
-  def __init__(self, data, next_node=None, position=0):
+  def __init__(self, data, position, next_node=None):
     self.data = data
     self.next_node = next_node # first node in list has nothing to point at
     self.position = position
@@ -23,7 +23,10 @@ class Node(object):
   def set_next(self, new_next): 
   	self.next_node = new_next 
 
-  	#See more at: https://www.codefellows.org/blog/implementing-a-singly-linked-list-in-python/#sthash.OSXodfIP.dpuf
+  def set_position(self, position):
+  	self.position = position
+
+# See more at: https://www.codefellows.org/blog/implementing-a-singly-linked-list-in-python/#sthash.OSXodfIP.dpuf
 
 # a linked list is a string of nodes, sort of like a string of pearls, 
 # with each node containing both data and a reference to the next node 
@@ -31,70 +34,49 @@ class Node(object):
 
 class LinkedList:
 
-    def __init__(self, head=None):
+    def __init__(self, head = None):
         self.head = head
 
-    def add_node(self, data):
-        new_node = Node(data) # create a new node
+    def add_node(self, data, position = 0):
+        new_node = Node(data, position) # create a new node
+        if not self.head is None:
+        	newpos = self.head.position + 1
+        	new_node.set_position(newpos)
         new_node.set_next(self.head) # link the new node to the 'previous' node.
-        
         self.head = new_node #  set the current node to the new one.
-        # need to give it a position?
-
-    def list_print(self):
-        node = self.cur_node 
-        while node:
-            #print node.data
-            node = node.next_node
 
     def size(self): 
     	current = self.head 
     	count = 0
     	while current != None: 
-    		count += 1 
+    		count += 1
     		current = current.get_next() 
     	return count 
 
-    def get_node_by_index(self, index):
-     	current = self.head # starts at 0
-    	while current != None:
-    		print current.position
-    		current = current.get_next()
-	 #        if current.position == index:
-	 #            return current.data
-	 #        else:
-	 #            current = current.get_next
-		# print "item not present in list"
-
-    # 	# cycle through until index number in list
-    # 	for 
-    # 	while current != None: 
-    # 		count += 1 
-    # 		current = current.get_next() 
-    # 	return count
+    def index(self, item):
+	    current = self.head
+	    while current != None:
+	        if current.position == item:
+	        	return current.get_data()
+	        else:
+	            current = current.get_next()
+	    print ("An error has occurred.")
 
 
 # Function to get the nth node from the last of a linked list
 
 # build a sample list
 ll = LinkedList()
-ll.add_node(6)
-ll.add_node(3)
-ll.add_node(1)
-ll.add_node(8)
-ll.add_node(4)
+ll.add_node(9)
+ll.add_node(122)
+ll.add_node(1333)
+ll.add_node(85555)
+ll.add_node(45555)
 
 m = 3
 
 def Question5(ll, m):
-	# ll.list_print() # works
-	# find the total length of the sample list
-	lenll = ll.size()
-	newspot = lenll - m
-	# for n in ll:
-	# 	print n
-	# need a "get all nodes"...
-	print ll.get_node_by_index(newspot)
+	print ll.index(m)
 
  
 Question5(ll, m)
